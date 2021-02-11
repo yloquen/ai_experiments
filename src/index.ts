6b11377b5a36b75d932202612d59294e27996e74
Ivan Kostov <i.kostov@wienergames.com>
2021-02-11 23:42:29 +0200

    progress

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-const LAYERS = [2,3,4,3];
+const LAYERS = [2,3,3,1];
 
 const activations = [];
 const sigmoidDerivatives = [];
 let weights = [];
 const weightVector = [];
 const biasVector = [];
-const biases = [];
+let biases = [];
 const dEdZ = [];
 let cnt = 0;
 
@@ -30,11 +30,13 @@ function init()
         weights[layerIdx] = [];
         weightVector[layerIdx] = [];
         biases[layerIdx] = [];
+        biasVector[layerIdx] = [];
         dEdZ[layerIdx] = [];
 
         for (let neuronIdx=0; neuronIdx < numNeurons; neuronIdx++)
         {
-            biases[layerIdx][neuronIdx] = 0;
+            biases[layerIdx][neuronIdx] = -.5 + Math.random();
+            biasVector[layerIdx][neuronIdx] = 0;
             activations[layerIdx][neuronIdx] = 0;
             sigmoidDerivatives[layerIdx][neuronIdx] = 0;
             if (layerIdx < LAYERS.length-1)
@@ -46,7 +48,7 @@ function init()
                 weightVector[layerIdx].push(wv);
                 for (let nextNeuronIdx=0; nextNeuronIdx < nextNumNeurons; nextNeuronIdx++)
                 {
-                    w.push(rnd(1));
+                    w.push(-.5 + Math.random());
                     wv.push(0);
                 }
             }
@@ -61,22 +63,15 @@ function forwardPass(inputs)
 
     for (let layerIdx = 0; layerIdx < activations.length-1; layerIdx++)
     {
-        let zSum=0;
         for (let inIdx = 0; inIdx < activations[layerIdx+1].length; inIdx++)
         {
             let z = 0;
             for (let outIdx = 0; outIdx < activations[layerIdx].length; outIdx++)
             {
-                if (layerIdx === 1 && inIdx === 1 && outIdx ===0)
-                {
-                    // weights[layerIdx][outIdx][inIdx] += .0001;
-                }
                 z += activations[layerIdx][outIdx] * weights[layerIdx][outIdx][inIdx];
             }
             z += biases[layerIdx+1][inIdx];
-            zSum += z;
-            let sigmoidVal = sigmoid(z);
-
+            const sigmoidVal = sigmoid(z);
             activations[layerIdx+1][inIdx] = sigmoidVal;
             sigmoidDerivatives[layerIdx+1][inIdx] = sigmoidVal * (1 - sigmoidVal);
         }
@@ -112,6 +107,7 @@ function backPass(targetOutputs)
                 const d = dEdZ[layerIdx][neuronIdx];
                 const w = weights[layerIdx-1][weightIdx][neuronIdx];
                 weightVector[layerIdx-1][weightIdx][neuronIdx] = d * activations[layerIdx-1][weightIdx];
+                biasVector[layerIdx][neuronIdx] = d;
                 sum += d*w;
             }
             dEdZ[layerIdx-1][weightIdx] = sum  * sigmoidDerivatives[layerIdx-1][weightIdx];
@@ -119,35 +115,48 @@ function backPass(targetOutputs)
 
         layerIdx--;
     }
-
-    debugger;
 }
 
 
-function test()
+function test(inputs, outputs)
 {
-    const deltaWeight = .00000000001;
+    const delta = .0001;
 
+    let s = "";
     for (let layerIdx=0; layerIdx < weights.length; layerIdx++)
     {
-        document.write("<br/>");
+        s += "<br/>";
         for (let inIdx=0; inIdx < weights[layerIdx].length; inIdx++)
         {
             for (let weightIdx=0; weightIdx < weights[layerIdx][inIdx].length; weightIdx++)
             {
-                document.write("W(" + layerIdx + ")" + inIdx + "," + weightIdx + ".........");
+                s += ("W(" + layerIdx + ")" + inIdx + "," + weightIdx + ". . . . .");
                 const weightsCopy = JSON.parse(JSON.stringify(weights));
-                forwardPass([1,0]);
-                const error = calcError([0,0,0]);
-                weights[layerIdx][inIdx][weightIdx] += deltaWeight;
-                forwardPass([1,0]);
-                const deltaError = calcError([0,0,0]) - error;
-                const derivative = deltaError/deltaWeight;
-                document.write(derivative.toFixed(4) + " : " + weightVector[layerIdx][inIdx][weightIdx].toFixed(4) + "<br/>");
+                forwardPass(inputs);
+                const error = calcError(outputs);
+                weights[layerIdx][inIdx][weightIdx] += delta;
+                forwardPass(inputs);
+                const deltaError = calcError(outputs) - error;
+                const derivative = deltaError/delta;
+                s += (derivative.toFixed(8) + " : " + weightVector[layerIdx][inIdx][weightIdx].toFixed(8) + "<br/>");
                 weights = weightsCopy;
             }
+
+            s += ("B(" + layerIdx + ")" + inIdx + ". . . . .");
+            const biasesCopy = JSON.parse(JSON.stringify(biases));
+            forwardPass(inputs);
+            const error = calcError(outputs);
+            biases[layerIdx][inIdx] += delta;
+            forwardPass(inputs);
+            const deltaError = calcError(outputs) - error;
+            const derivative = deltaError/delta;
+            s += (derivative.toFixed(8) + " : " + biasVector[layerIdx][inIdx].toFixed(8) + "<br/>");
+            biases = biasesCopy;
         }
+
     }
+
+    return s;
 }
 
 
@@ -162,19 +171,90 @@ function calcError(targetOutputs)
     return totalError;
 }
 
+
+function train(rate)
+{
+    for (let i=0; i < 1000; i++)
+    {
+        const batchSize = 1;
+        const batchSizeReciprocal = 1 / batchSize;
+
+        for (let batchIndex = 0; batchIndex < batchSize; batchIndex++)
+        {
+            const ins = generateIns();
+            const outs = generateOuts(ins);
+            forwardPass(ins);
+            backPass(outs);
+        }
+
+        for (let layerIdx=0; layerIdx < weights.length; layerIdx++)
+        {
+            for (let inIdx = 0; inIdx < weights[layerIdx].length; inIdx++)
+            {
+                for (let weightIdx = 0; weightIdx < weights[layerIdx][inIdx].length; weightIdx++)
+                {
+                    weights[layerIdx][inIdx][weightIdx] += weightVector[layerIdx][inIdx][weightIdx] * batchSizeReciprocal * rate;
+                    weightVector[layerIdx][inIdx][weightIdx] = 0;
+                }
+                biases[layerIdx][inIdx] += biasVector[layerIdx][inIdx] * batchSizeReciprocal * rate ;
+                biasVector[layerIdx][inIdx] = 0;
+            }
+        }
+    }
+}
+
+
+function generateIns()
+{
+    return [Number(Math.random() > .5), Number(Math.random() > .5)];
+}
+
+
+function generateOuts(ins)
+{
+    return [Number(ins[0] !== ins[1])];
+}
+
+
 init();
 
-forwardPass([1,0]);
-backPass([0,0,0]);
-
-test();
 
 
 
+//train(.1);
+//forwardPass([0,0]);
+
+
+let cnt2 = 0;
+
+document.getElementById('submitBut').onclick = () =>
+{
+    const inputs = document.getElementsByClassName('input');
+    const inArr = [];
+    for (let i = 0; i < inputs.length; i++)
+    {
+        const input = inputs[i];
+        inArr.push(input.value);
+    }
+
+    forwardPass(inArr);
+    backPass(generateOuts(inArr));
+    let s = test(inArr, generateOuts(inArr));
+
+    activations[activations.length-1].forEach( (v,i) => s += ("OUT " + i + " > " + v.toFixed(4) + "\n"));
+    s += "<br/>Error : " + calcError(generateOuts(inArr));
+    document.getElementById("result").innerHTML = s;
+
+
+};
 
 
 
+//forwardPass([1,0]);
+//backPass([0]);
+//test();
 
+train(.1);
 
-
+//debugger;
 
